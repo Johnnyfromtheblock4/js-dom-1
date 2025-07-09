ee96b326a1a8d49e9f1862051088504a0f2c1e86
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-07-09 18:03:07 +0200

    [UPD] lampadina-accesa

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,8 @@
 const button = document.getElementById("my-btn");
 const img = document.getElementById("pic");
 
+button.addEventListener("click", function () {
+  if (img.classList.contains("spenta")) {
+    img.setAttribute("src", "assets/img/yellow_lamp.png");
+  }
+});
