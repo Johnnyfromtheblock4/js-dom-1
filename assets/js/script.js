3d4dfa18270663632add4e8fbba2f856b1e10d73
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-07-09 18:42:58 +0200

    [UPD] bonus-spegni

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,5 +5,6 @@ const img = document.getElementById("pic");
 button.addEventListener("click", function () {
   if (img.classList.contains("spenta")) {
     img.setAttribute("src", "assets/img/yellow_lamp.png");
+    button.innerHTML = "Spegni"
   }
 });
