6fb28b0eee728c53d3f7aaccd6903395722d18fe
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-07-09 19:26:30 +0200

    [UPD] fine-bonus

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,11 @@ const button = document.getElementById("my-btn");
 const img = document.getElementById("pic");
 
 button.addEventListener("click", function () {
-  if (img.classList.contains("spenta")) {
-    img.setAttribute("src", "assets/img/yellow_lamp.png");
-    button.innerHTML = "Spegni"
+  if (button.innerHTML.includes(`Accendi`)) {
+    img.setAttribute(`src`, `assets/img/yellow_lamp.png`);
+    button.innerHTML = `Spegni`;
+  } else {
+    img.setAttribute(`src`, `assets/img/white_lamp.png`);
+    button.innerHTML = `Accendi`;
   }
 });
