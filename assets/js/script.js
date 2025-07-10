b042e1c632262ea344812500360aa4902e355517
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-07-10 09:56:56 +0200

    [UPD] correzione-esercizio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,23 @@
-//imposto le variabili
+// //imposto le variabili
 const button = document.getElementById("my-btn");
 const img = document.getElementById("pic");
 
+// button.addEventListener("click", function () {
+//   if (button.innerHTML.includes(`Accendi`)) {
+//     img.setAttribute(`src`, `assets/img/yellow_lamp.png`);
+//     button.innerHTML = `Spegni`;
+//   } else {
+//     img.setAttribute(`src`, `assets/img/white_lamp.png`);
+//     button.innerHTML = `Accendi`;
+//   }
+// });
+
 button.addEventListener("click", function () {
-  if (button.innerHTML.includes(`Accendi`)) {
-    img.setAttribute(`src`, `assets/img/yellow_lamp.png`);
-    button.innerHTML = `Spegni`;
+  if (img.src.includes(`yellow_lamp.png`)) {
+    img.src = `assets/img/white_lamp.png`;
+    button.innerText = `Accendi`;
   } else {
-    img.setAttribute(`src`, `assets/img/white_lamp.png`);
-    button.innerHTML = `Accendi`;
+    img.src = `assets/img/yellow_lamp.png`;
+    button.innerText = `Spegni`;
   }
 });
